ea9c35046de3a9ba64d4733842ec3b29bfa1b287
krishnayah <139855911+krishnayah@users.noreply.github.com>
2024-06-27 02:27:27 +0000

    Deploying to gh-pages from @ krishnayah/krishnayah.github.io@5c3dbad89f289e57e8fa46c63506ac36130ce433 🚀

--- _app/immutable/chunks/skills.8e7092f2.js
+++ _app/immutable/chunks/skills.8e7092f2.js
@@ -1,4 +1,4 @@
-import{A as n}from"./assets.8062788d.js";import"./index.433bca53.js";const m=`# Svelte
+import{A as n}from"./assets.b06526fe.js";import"./index.433bca53.js";const m=`# Svelte
 
 ---
 
