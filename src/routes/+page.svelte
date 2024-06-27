003bd1cbb8eef6510f68f1b49f741afd3c7f489c
Jordan Krishnayah <139855911+krishnayah@users.noreply.github.com>
2024-06-26 22:39:01 -0400

    Modified front page, created future plans.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,14 +49,24 @@
 	<img src="https://avatars.githubusercontent.com/u/139855911?v=4" alt="hero" class="w-200px h-200px md:w-300px md:h-300px rounded-lg" />
 </div>
 
-<div class="row flex-1 justify-center items-center p-y-0px p-x-10px">
-	<div class="rotate-5">
-		<img src="https://res.cloudinary.com/krishnayahphotography/image/upload/DSCF9936_uckj5t" alt="hero" class="w-auto h-200px md:h-300px rounded-lg" />
-	</div>	
-	<div class="w-10">
-
+<a href="/photography">
+	<h1 class="my-10">My photography</h1>
+	
+	<div class="row flex-1 justify-center items-center p-y-0px p-x-10px">
+		<div class="rotate-5">
+			<img src="https://res.cloudinary.com/krishnayahphotography/image/upload/DSCF9936_uckj5t" alt="hero" class="w-auto h-200px md:h-300px rounded-lg" />
+		</div>	
+		<div class="w-5">
+	
+		</div>
+		<div class="-rotate-3" >
+			<img src="https://res.cloudinary.com/krishnayahphotography/image/upload/DSCF9985_cu4qyr" alt="hero" class="w-200px h-auto md:w-300px md:h-300px rounded-lg" />
+		</div>	
+	
+		<div class="w-5"/>
+	
+		<div class="rotate-5">
+			<img src="https://res.cloudinary.com/krishnayahphotography/image/upload/v1719295624/42830031_usxjcx.jpg" alt="hero" class="w-auto h-200px md:h-300px rounded-lg" />
+		</div>
 	</div>
-	<div class="-rotate-3">
-		<img src="https://res.cloudinary.com/krishnayahphotography/image/upload/DSCF9985_cu4qyr" alt="hero" class="w-200px h-auto md:w-300px md:h-300px rounded-lg" />
-	</div>	
-</div>
+</a>
